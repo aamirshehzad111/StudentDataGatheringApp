13131f8cca8c148b4f60b053e93c3d68fdb64ced
Aamir Shehzad <shakeel.arshad30@gmail.com>
2020-01-30 02:31:30 +0500

    Update and rename app.js to index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.get('/', (req, res) => {
+    
+    res.sendFile('/home/ec2-user/studentdataapp/index.html);
+});
+
+var server = app.listen(8999, () =>{
+
+    console.log('Server is running from port 8999');
+});
