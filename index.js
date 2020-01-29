cdfd97f04333ade84a121d013a6de2b7fe4949bc
Aamir Shehzad <shakeel.arshad30@gmail.com>
2020-01-30 02:57:17 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
-
+app.use(bodyParser.urlencoded({extended: false}));
 app.get('/', (req, res) => {
     
     res.sendFile('/home/ec2-user/studentdataapp/index.html');
+});
+
+app.post('/post-data', (req, res) => {
+    var obj= {
+    
+        name : req.body.sname,
+        fname : req.body.fname,
+        uni : req.body.uni,
+        dept : req.body.dept,
+        degree : req.body.deg,
+        cgpa : req.body.cgpa
+    };
+    
+    res.send(obj.toString());
+    
 });
 
 var server = app.listen(8999, () =>{
