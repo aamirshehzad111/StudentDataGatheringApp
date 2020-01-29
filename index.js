69596d6a4ff01998a9df71006bbf0b3d70d18c90
Aamir Shehzad <shakeel.arshad30@gmail.com>
2020-01-30 01:33:40 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('hello codepipeline');
+#!/bin/bash
+# Install node.js and Forever.js
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+sudo yum install nodejs -y
+sudo npm install forever -g
